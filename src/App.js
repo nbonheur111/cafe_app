6655db016575715621b886bcd9207d52e4ce455c
nbonheur111 <56658293+nbonheur111@users.noreply.github.com>
2023-02-10 13:50:30 -0600

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import AuthPage from './pages/auth'
+import NewOrderPage from './pages/new_order'
+import OrderHistoryPage from './pages/order_history'
+import { Routes, Route } from 'react-router-dom'
+
+
 
 function App() {
+  const [user, setUser] = useState(null)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user?
+        
+        <Routes>
+            <Route path= "/order" element= {<OrderHistoryPage />} /> 
+          <Route path= "/order/new" element= {<NewOrderPage />} /> 
+        </Routes>
+        :
+        <AuthPage />
+    
+      }
+
+     
+    
     </div>
   );
 }
